79cf8cb6513b9fe2c7de78cb5167ea5a3b48872f
FranciscoFernandoParraPenagos1164 <francisco.parra.penagos@gmail.com>
2022-06-28 23:03:47 -0500

    final commit

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 window.onload = () => {
     const lista = document.querySelectorAll('.list');
     const sections = document.querySelectorAll('section');
+    const button = document.getElementById('menu-responsive-button');
+    const icon = document.getElementsByClassName('menu-responsive')
+    const menu = document.getElementById('asideMenuList');
+    const header = document.getElementById('inicio');
+    const content = document.getElementById('body-content');
     const observer = new IntersectionObserver((entradas, observer) => {
         entradas.forEach((entrada) => {
             if (entrada.isIntersecting) {
@@ -22,12 +27,40 @@ window.onload = () => {
                 }
             }
         })
-    }, { threshold: 0.5 });
+    }, { threshold: 0.55 });
 
     sections.forEach((section) => observer.observe(section));
+
+    button.addEventListener('click', () => {
+        if (icon[0].classList.item(1) != "menu-active") {
+            openMenu();
+        } else if (icon[0].classList.item(1) == "menu-active") {
+            closMenu();
+        }
+    });
+
+    header.addEventListener('click', () => {
+        closMenu();
+    });
+
+    content.addEventListener('click', () => {
+        closMenu();
+    });
 
     function offButton() {
         lista.forEach((item) =>
             item.classList.remove("active"));
+    }
+
+    function openMenu() {
+        icon[0].classList.add('menu-active');
+        menu.classList.add('menu-open');
+        menu.classList.remove('menu-close');
+    }
+
+    function closMenu() {
+        icon[0].classList.remove('menu-active');
+        menu.classList.remove('menu-open');
+        menu.classList.add('menu-close');
     }
 }
